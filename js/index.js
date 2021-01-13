20fdb572d86abe7522c0fc9554b507ae6e53b7a6
Shlomi Fish <shlomif@shlomifish.org>
2021-01-13 09:43:51 +0200

    try2restore music

--- js/index.js
+++ js/index.js
@@ -6,21 +6,21 @@
  * So I created this one.
  *
  * I wrote an article where I explain how this works:
- * http://timpietrusky.com/star-wars-opening-crawl-from-1977
+ * https://timpietrusky.com/star-wars-opening-crawl-from-1977
  * 
  * Watch the Start Wars opening crawl on YouTube.
- * http://www.youtube.com/watch?v=7jK-jZo6xjY
+ * https://www.youtube.com/watch?v=7jK-jZo6xjY
  * 
  * Stuff I used:
  * - CSS (animation, transform)
  * - HTML audio (the opening theme)
  * - SVG (the Star Wars logo from wikimedia.org)
- *   http://commons.wikimedia.org/wiki/File:Star_Wars_Logo.svg
+ *   https://commons.wikimedia.org/wiki/File:Star_Wars_Logo.svg
  * - JavaScript (to sync the animation/audio)
  *
  * Thanks to Craig Buckler for his amazing article 
  * which helped me to create this remake of the Star Wars opening crawl. 
- * http://www.sitepoint.com/css3-starwars-scrolling-text/ 
+ * https://www.sitepoint.com/css3-starwars-scrolling-text/ 
  *
  * Sound copyright by The Walt Disney Company.
  * 
